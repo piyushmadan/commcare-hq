fb6789e06e095ca61a8a52292add1e536d30aca2
Nick Pellegrino <npellegrino@dimagi.com>
2014-02-27 15:47:08 -0600

    add from address

--- corehq/apps/accounting/management/commands/test_invoicing.py
+++ corehq/apps/accounting/management/commands/test_invoicing.py
@@ -1,5 +1,5 @@
 from django.core.management import BaseCommand
-from corehq.apps.accounting.invoicing import InvoiceTemplate
+from corehq.apps.accounting.invoicing import InvoiceTemplate, Address
 
 
 class Command(BaseCommand):
@@ -9,7 +9,18 @@ class Command(BaseCommand):
         filename = 'my_test.pdf'
 
         try:
-            generator = InvoiceTemplate(filename)
+            from_address = Address(first_line='585 Massachusetts Ave',
+                                   second_line='Suite 3',
+                                   city='Cambridge',
+                                   region='MA',
+                                   postal_code='02139',
+                                   country='USA',
+                                   phone_number='(617) 649-2214',
+                                   email_address='admin@dimagi.com',
+                                   website='http://dimagi.com')
+
+            generator = InvoiceTemplate(filename,
+                                        from_address=from_address)
             generator.get_pdf()
 
             print "%s generated" % filename
