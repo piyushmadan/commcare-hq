1b089e99dd826ebe4ee8868dcf7a4f214e39fe4b
Biyeun Buczyk <biyeun@dimagi.com>
2014-05-22 17:15:58 -0400

    Add a class-based view for adding/editing gateways and make sure the forms use crispy forms.

--- corehq/apps/mach/forms.py
+++ corehq/apps/mach/forms.py
@@ -1,9 +1,26 @@
 from django.forms.fields import *
 from corehq.apps.sms.forms import BackendForm
 from dimagi.utils.django.fields import TrimmedCharField
+from crispy_forms import layout as crispy
+from django.utils.translation import ugettext_lazy as _
+
 
 class MachBackendForm(BackendForm):
-    account_id = TrimmedCharField()
-    password = TrimmedCharField()
-    sender_id = TrimmedCharField()
+    account_id = TrimmedCharField(
+        label=_("Account ID"),
+    )
+    password = TrimmedCharField(
+        label=_("Password"),
+    )
+    sender_id = TrimmedCharField(
+        label=_("Sender ID"),
+    )
 
+    @property
+    def gateway_specific_fields(self):
+        return crispy.Fieldset(
+            _("Syniverse Settings"),
+            'account_id',
+            'password',
+            'sender_id',
+        )
--- corehq/apps/twilio/forms.py
+++ corehq/apps/twilio/forms.py
@@ -1,9 +1,30 @@
 from django.forms.fields import *
 from corehq.apps.sms.forms import BackendForm
 from dimagi.utils.django.fields import TrimmedCharField
+from crispy_forms import layout as crispy
+from django.utils.translation import ugettext_lazy as _
+
 
 class TwilioBackendForm(BackendForm):
-    account_sid = TrimmedCharField()
-    auth_token = TrimmedCharField()
-    phone_number = TrimmedCharField()
+    account_sid = TrimmedCharField(
+        label=_("Account SID"),
+    )
+    auth_token = TrimmedCharField(
+        label=_("Auth Token"),
+    )
+    phone_number = TrimmedCharField(
+        label=_("Phone Number"),
+        help_text=_(
+            "For short codes, just enter the short code. For phone numbers, "
+            "enter the number in international format including the '+' sign."
+        )
+    )
 
+    @property
+    def gateway_specific_fields(self):
+        return crispy.Fieldset(
+            _("Twilio Settings"),
+            'account_sid',
+            'auth_token',
+            'phone_number',
+        )
